54f2a882ecce865f3d78fe9d36d1b22a27e2c8e9
KakinumaTakashi <homesapplications@gmail.com>
2017-07-26 11:50:53 +0900

    2017/7/26 update 不要なクラスを削除

--- app/src/main/java/jp/ecweb/homes/a1601/network/HttpCategoryListener.java
+++ app/src/main/java/jp/ecweb/homes/a1601/network/HttpCategoryListener.java
@@ -5,7 +5,6 @@ import jp.ecweb.homes.a1601.models.Category;
 /**
  * カテゴリ一覧コールバック
  */
-// TODO 材料カテゴリと共通化
 public interface HttpCategoryListener {
     void onSuccess(Category category);
     void onError();
